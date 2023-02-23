070043cb89d30c7119b532e6b93e85559d6ec13a
Nirmal Shah <nirmalshah2000@gmail.com>
2023-02-22 23:37:47 -0800

    Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyB75kR-nYFJHnOFp07ZbdRXsqA6GSoJKbM",
-  authDomain: "clone-2be8a.firebaseapp.com",
-  projectId: "clone-2be8a",
-  storageBucket: "clone-2be8a.appspot.com",
-  messagingSenderId: "228712449854",
-  appId: "1:228712449854:web:2c2442e2bb3df475668e6f",
-  measurementId: "G-TX8703WK88",
+  apiKey: "AIzaSyBIX9M_nuW1vleE6-2qPZyzDRPjwanmVlw",
+  authDomain: "clone-86285.firebaseapp.com",
+  projectId: "clone-86285",
+  storageBucket: "clone-86285.appspot.com",
+  messagingSenderId: "278764950102",
+  appId: "1:278764950102:web:0ad8af14043f2e3605e1f7",
+  measurementId: "G-BXT1G5QREE",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Initialize Firebase Authentication and get a reference to the service
